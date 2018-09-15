c52cb3a4ae76660a26d3c680124bc7d7393f9532
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2018-09-15 01:56:07 -0700

    revised fib while example output

--- fib_while.cpp
+++ fib_while.cpp
@@ -12,11 +12,11 @@ int main(int argn, char *argv[]){
     while ((upper_bound > a2) && (max_iter > i)){
         i ++;
         a2 = a0 + a1;
-        std::cout << a2 << ' ';
 	a0 = a1;
 	a1 = a2;
+        std::cout << a0 << ' ';
     }
 
-    std::cout << "\nLargest Fibo below " << upper_bound << "= " << a1 << '\n';
+    std::cout << "\nLargest Fibo below " << upper_bound << "= " << a0 << '\n';
 }
 
