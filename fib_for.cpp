07614226fa050b2834cf335d65e49bd2856a90fa
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2018-09-14 22:31:51 -0700

    fib for : maximum below

--- fib_for.cpp
+++ fib_for.cpp
@@ -4,26 +4,20 @@ int main(int argn, char *argv[]){
 
     const int max_iter = 10000;
 
-    const int n = 10;
-    int found = 2, fib[10] = {0, 1};
+    const int upper_bound = 100;
+    int a0 = 0, a1 = 1, a2 = 0;
 
-    for (int i=0; max_iter > i; ++i){
-        fib[found] = fib[found-1] + fib[found-2];
-	++found;
-	if (n <= found){
-	    break;
-	}
+    for (int i=0; (max_iter > i); ++i){
+        a2 = a0 + a1;
+        if (upper_bound < a2){
+            break;
+        }
+        a0 = a1;
+        a1 = a2;
+        std::cout << a2 << ' ';
     }
-    /* // alternatively
-    for (int i=0; (max_iter > i) && (n > found); ++i, ++found){
-        fib[found] = fib[found-1] + fib[found-2];
-    }*/
 
-    std::cout << "fib = [";
-    for (int j=0; n > j; ++j){
-	std::cout << fib[j] << ", ";
-    }
-    std::cout << "]\n";
+    std::cout << "\nLargest Fibo below " << upper_bound << "= " << a1 << "\n";
 
 }
 
