9151c5553a7014b48e7de343a0a5c03994cfede2
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2018-09-14 22:34:31 -0700

    Added fib while

---
+++ fib_while.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(int argn, char *argv[]){
+
+    const int max_iter =10000;
+    const int upper_bound = 100;
+    
+    int a0 = 0, a1 = 1, a2 = 0;
+    
+    int i = 0;
+
+    while ((upper_bound > a2) && (max_iter > i)){
+        i ++;
+        a2 = a0 + a1;
+        std::cout << a2 << ' ';
+	a0 = a1;
+	a1 = a2;
+    }
+
+    std::cout << "\nLargest Fibo below " << upper_bound << "= " << a1 << '\n';
+}
+
