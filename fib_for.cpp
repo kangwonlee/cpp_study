095e1171abdf9da53e68bf501f44e0b62711796c
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2018-09-14 19:45:16 -0700

    Added fibonacci for example

---
+++ fib_for.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(int argn, char *argv[]){
+
+    const int max_iter = 10000;
+
+    const int n = 10;
+    int found = 2, fib[10] = {0, 1};
+
+    for (int i=0; (max_iter > i) && (n > found); ++i, ++found){
+        fib[found] = fib[found-1] + fib[found-2];
+    }
+
+    std::cout << "fib = [";
+    for (int j=0; n > j; ++j){
+	std::cout << fib[j] << ", ";
+    }
+    std::cout << "]\n";
+
+}
+
