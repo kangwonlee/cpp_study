44e1080a1c16a68909f0a9642b021db3dc43a576
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2018-09-15 11:37:11 -0700

    specified namespace std to make lines shorter

--- make_md_table.cpp
+++ make_md_table.cpp
@@ -3,9 +3,11 @@
 // https://stackoverflow.com/questions/7349689/how-to-print-using-cout-the-way-a-number-is-stored-in-memory
 #include <bitset>
 
+using namespace std;
+
 int main(int argn, char *argv[]){
 
-	std::ofstream ofs("ascii_table.md", std::ofstream::out);	
+	ofstream ofs("ascii_table.md", std::ofstream::out);	
 	char elipsis_line[] = " ... | ... | ... \n";
 	const int n = 8;
 
@@ -13,27 +15,27 @@ int main(int argn, char *argv[]){
 	ofs << ":-----------------:|:--------:|:------:\n";
 
 	for (int i=0; i<(8+1); ++i){
-		ofs << std::bitset<n>(i) << " | " << i << " | " << char(i) << '\n';
+		ofs << bitset<n>(i) << " | " << i << " | " << char(i) << '\n';
 	}
 	ofs << elipsis_line;
 
         for (int i=14; i<(50+1); ++i){
-                ofs << std::bitset<n>(i) << " | " << i << " | " << char(i) << '\n';
+                ofs << bitset<n>(i) << " | " << i << " | " << char(i) << '\n';
         }
         ofs << elipsis_line;
 
         for (int i=57; i<(65+1); ++i){
-                ofs << std::bitset<n>(i) << " | " << i << " | " << char(i) << '\n';
+                ofs << bitset<n>(i) << " | " << i << " | " << char(i) << '\n';
         }
         ofs << elipsis_line;
 
 	for (int i=90; i<(97+1); ++i){
-                ofs << std::bitset<n>(i) << " | " << i << " | " << char(i) << '\n';
+                ofs << bitset<n>(i) << " | " << i << " | " << char(i) << '\n';
         }
         ofs << elipsis_line;
 
 	for (int i=122; i<(127+1); ++i){
-                ofs << std::bitset<n>(i) << " | " << i << " | " << char(i) << '\n';
+                ofs << bitset<n>(i) << " | " << i << " | " << char(i) << '\n';
         }
         ofs << elipsis_line;
 
