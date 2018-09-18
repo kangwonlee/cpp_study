3a5ba9435101fde996cc17a5b4c8d06f685dc5c7
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2018-09-18 16:42:34 -0700

    Using bitset and long int *: smallerst float using C++ for

--- smallest_float.cpp
+++ smallest_float.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
+#include <bitset>
 
 int main(int argn, char *argv[]){
 	double a = 1.0;
+	const int n = sizeof(a) * 8;
+	unsigned long int * lp = (unsigned long int *) & a;
 
 	for (int counter=0;counter < 1e10; ++counter){
 		a *= 0.5;
-		std::cout << "(" << counter << ") " << a << '\n';
+		std::cout << "(" << counter << ") " 
+			  << a 
+			  << "[" << std::bitset<n>(*lp) << "]"
+			  << '\n';
 		if (!(0<a)){
 			break;
 		}
