c1c4d4042143b9a969d8dbab7ff4f7aecc3ad86d
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2018-09-26 22:59:47 +0700

    Corrected smallest floating point example

--- smallest_float_while.cpp
+++ smallest_float_while.cpp
@@ -1,65 +1,18 @@
-!git add *.s
-!git commit -m "added assembly"
-!make && ./hello
-s/cout << "/printf("
-s/cout/printf
-%s/cout/printf(
-!g++ condition.cpp && ./a.out
-!g++ if_else_if.cpp && ./a.out
-!g++ switch_case.cpp && ./a.out
-!/a.out
-!g++ -Wall -g for_sum.cpp
-!g++ -Wall -g while_sum.cpp && ./a.out
-git add -p
-git ad
-!git add while_sum.cpp
-!git commit -m "Added while example"
-! ./a.out
-!g++ -Wall -g fib_for.cpp
-!g++ -Wall -g fib_for.cpp && ./a.out
-!g++ -Wall -g fib_while.cpp && ./a.out
-!g++ -Wall -g cpp_func.cpp
-!g++ -Wall -g cpp_func.cpp && ./a.out
-!g++ -Wall -g cpp_scope.cpp && ./a.out
-!ls *.cpp
-!g++ make_md_table.cpp
-"w
-!g++ -g -Wall make_md_table.cpp
-!cat ascii_table.md
-git commit -m "Used ofstream and bitset"
-!git commit -m "Used ofstream and bitset"
-%s/std::/
-!g++ -g -Wall make_md_table.cpp && ./a.out
-!g++ -Wall -g type_conv.cpp && ./a.out
-!git add -p
-!git add cpp_string.cpp
-!git commit -m "Added C++ string example"
-!g++ -Wall -g cpp_string.cpp && ./a.out
-help
-w cpp_chg_str.cpp
-!g++ -Wall -g cpp_chg_str.cpp && ./a.out
-!which valgrind
-!which gprof
-!git status
-w cpp_vector.cpp
-!g++ -Wall -g cpp_vector.cpp
-!g++ -Wall -g cpp_vector.cpp && ./a.out
-!g++ -Wall -f cpp_map.cpp && ./a.out
-!g++ -Wall -g cpp_map.cpp && ./a.out
-!git diff
-!g++ -Wall -g cpp_matrix.cpp && ./a.out
-!ls
-!git add cpp_matrix.cpp
-!git commit -m "Started C++ matrix class"
-wq
-q!
-!g++ -Wall -g smallest_float.cpp
-!./a.out
-!g++ -Wall -g smallest_float.cpp && a.out
-!git commit -m "Started smallerst float using C++ for"
-!git add smallest_float.cpp
-!git commit -m "Using bitset and long int *: smallerst float using C++ for"
-q
-w
-!g++ -Wall -g smallest_float.cpp && ./a.out
+#include <iostream>
+
+int main(int argn, char *argv[]){
+	double a = 1.0;
+	int counter = 0;
+
+	while (0<a){
+		a *= 0.5;
+		std::cout << "(" << (++counter) << ") " 
+			  << a 
+			  << '\n';
+		if (10000<counter){
+			break;
+		}
+	}
+	return 0;
+}
 
