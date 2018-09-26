66f1a24f613a616ddb635a1cf1915b589fbf3930
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2018-09-26 23:40:25 +0700

    Added reassigning pointer

--- cpp_ptr_00.cpp
+++ cpp_ptr_00.cpp
@@ -4,12 +4,21 @@
 using namespace std;
 
 int main(int argn, char *argv[]){
-    int i = 0;
+    int i = 0, j = 0x01020304;
     int *p = &i;
-    
+
     cout << "i  = " << setw(16) <<  i << '\n';
     cout << "&i = " << setw(16) << &i << '\n';
-    
+
+    cout << "*p = " << setw(16) << *p << '\n';
+    cout << "p  = " << setw(16) <<  p << '\n';
+    cout << "&p = " << setw(16) << &p << '\n';
+
+    p = &j;
+
+    cout << "j  = " << setw(16) <<  j << '\n';
+    cout << "&j = " << setw(16) << &j << '\n';
+
     cout << "*p = " << setw(16) << *p << '\n';
     cout << "p  = " << setw(16) <<  p << '\n';
     cout << "&p = " << setw(16) << &p << '\n';
