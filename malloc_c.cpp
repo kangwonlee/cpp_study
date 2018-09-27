6a06f8080ec82e96c286f6a3210012956a8ffc22
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2018-09-27 16:24:45 +0700

    Added C memory management examples

---
+++ malloc_c.cpp
@@ -0,0 +1,38 @@
+#include <iomanip>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(const int argn, const char * argv[]){
+    const int n = 16;
+    int * m = (int *) malloc(sizeof(int) * n);
+    int * c = (int *) calloc(n, sizeof(int));
+
+    // must check if OS could successfully allocate memory
+    if (!m){
+        cout << "Unable to allocate m = malloc(" << (sizeof(int) * n)
+             << ")\n";
+        exit(EXIT_FAILURE);
+    }
+
+    if (!c){
+        cout << "Unable to allocate c = calloc(" << n
+             << ", " << sizeof(int)
+             << ")\n";
+        exit(EXIT_FAILURE);
+    }
+    
+    for (int i=0; n > i; ++i){
+        cout << "m[" << setw(2) << dec << i << "] = " 
+	     << hex << m[i] << '\t';
+        cout << "c[" << setw(2) << dec << i << "] = " 
+	     << hex << c[i] << '\n';
+    }
+
+    free(c);
+    free(m);
+
+    return 0;
+}
+
