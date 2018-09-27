c99718617a35f7e4ff8e442fe6d3b532ca5c0c42
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2018-09-27 16:29:30 +0700

    Added C++ new delete example

---
+++ new_delete.cpp
@@ -0,0 +1,45 @@
+#include <iomanip>
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(const int argn, const char * argv[]){
+    int n = 16;
+    int * i = new int;
+    int * m = new int [n];
+    int * c = new int [n]();
+
+    // must check if OS could successfully allocate memory
+    if (!i){
+        cout << "Unable to allocate i = new int\n";
+        exit(EXIT_FAILURE);
+    }
+
+    if (!m){
+        cout << "Unable to allocate m = new int [" 
+             << (sizeof(int) * n)
+             << "]\n";
+        exit(EXIT_FAILURE);
+    }
+
+    if (!c){
+        cout << "Unable to allocate c = new int [" 
+             << n << ", " << sizeof(int)
+             << "]()\n";
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i=0; n > i; ++i){
+        cout << "m[" << setw(2) << dec << i << "] = "
+             << hex << m[i] << '\t';
+        cout << "c[" << setw(2) << dec << i << "] = "
+             << hex << c[i] << '\n';
+    }
+
+    free(c);
+    free(m);
+
+    return 0;
+}
+
