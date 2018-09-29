6478f7678ba967db3542078e860ee787e8f1d512
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2018-09-29 15:16:55 +0700

    Reorganized display

--- hello.cpp
+++ hello.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 #include <cstdio>
 
-
 int main(int argn, char * argv[]){
 
 	int a = int(argv[0][0]);
 	int b = int(argv[0][1]);
 	int c = 0;
 
-	printf("argv[0] = %s\n", argv[0]);	
-	printf("a, b = %d, %d\n", a, b);
+	printf("argv[0] = %s\n", argv[0]);
+	printf("argv[1] = %s\n", argv[1]);
+	printf("argv[2] = %s\n\n", argv[2]);
+
+	printf("a, b, c = %d, %d, %d\n", a, b, c);
 
 	b++;
-	puts("b++\n");
-	printf("a, b = %d, %d\n", a, b);
+	puts("\nb++");
+	printf("a, b, c = %d, %d, %d\n", a, b, c);
 
 	b+=a;
-	puts("b+=a\n");
-	printf("a, b = %d, %d\n", a, b);
+	puts("\nb+=a");
+	printf("a, b, c = %d, %d, %d\n", a, b, c);
 
 	c = a + b;
-	puts("c = a + b\n");
+	puts("\nc = a + b");
 	printf("a, b, c = %d, %d, %d\n", a, b, c);
 
 	return 0;
