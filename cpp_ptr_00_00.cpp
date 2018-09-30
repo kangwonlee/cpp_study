69c63652ab3e94485c06ee81868e2338e85dca32
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2018-09-30 07:58:15 -0700

    Separated C++ pointer example 00 of 03/00

---
+++ cpp_ptr_00_00.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argn, char *argv[]){
+    // This is an integer variable
+    int i = 0;
+
+    // This is a pointer variable to the variable i
+    int *p = &i;
+
+    // Value of the variable i
+    cout << "i  = " << setw(16) << hex <<  i << '\n';
+    // Address of the variable i
+    cout << "&i = " << setw(16) << &i << '\n';
+
+    // Integer value at the location p
+    cout << "*p = " << setw(16) << hex << *p << '\n';
+    // Value of the pointer variable p
+    cout << "p  = " << setw(16) <<  p << '\n';
+    // Address of the pointer variable p
+    cout << "&p = " << setw(16) << &p << '\n';
+
+    return 0;
+}
