a9ae6d4b2cd2b11c45e70cff281fb9712cb8c8e0
KangWon LEE <kangwon.lee@kpu.ac.kr>
2018-10-05 00:21:28 +0900

    vector.push_back()

--- cpp_vector.cpp
+++ cpp_vector.cpp
@@ -5,8 +5,11 @@ using namespace std;
  
 int main ()
 {
+
 	// http://www.cplusplus.com/reference/vector/vector/
-	vector<char> a ({'a', 'b', 'c', 'd'});
+	vector<char> a;
+	a.push_back('a'); a.push_back('b'); 
+	a.push_back('c'); a.push_back('d');
 
 	for (unsigned int i=0; i < a.size(); ++i){
 		cout << "a[" << i << "] = " << a[i] << "\n";
