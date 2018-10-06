55ac24d5a5a70bcb3463ac3dd150c33d0c81f79a
KangWon Lee (u64) <kangwon.lee@kpu.ac.kr>
2018-10-06 06:43:06 -0700

    Check memory location

--- account_module.h
+++ account_module.h
@@ -7,17 +7,20 @@ int32_t balance;
 
 void deposit (int32_t amount){
     std::cout << "Deposit " << amount << '\n';
+    std::cout << "to " << & balance << '\n';
     balance += amount;
 }
 
 
 void withdraw (int32_t amount){
-    std::cout << "Withdraw " << amount << '\n';
+    std::cout << "Withdraws " << amount << '\n';
+    std::cout << "from " << & balance << '\n';
     balance += -amount;
 }
 
 
 int32_t check(){
+    std::cout << "balance of " << & balance << '\n';
     return balance;
 }
 // End account_module.h
